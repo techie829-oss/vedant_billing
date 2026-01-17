80d2fa108d693b2f1de43a1a3f690bf978013170
Rohit K <rohitk@gullygroup.in>
2026-01-17 08:43:25 +0530

    chore: remove unused seeders and temporary files

--- frontend/fix_line23.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Fix line 23 - the broken address line
-sed -i '' '23s/.*/                                <p class="text-\[10px\] mt-0.5">{{ invoice.business?.meta?.address_line_1 || '\''Kewalpurwa Nagar'\'' }}<\/p>/' src/views/invoices/layouts/ClassicGridLayout.vue
